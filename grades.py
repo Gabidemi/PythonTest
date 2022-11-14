42d2df4bdd9499997b6afb9767ee49f59d009a29
Gabidemi <gabidemi@brooklynsteamcenter.org>
2022-11-14 10:35:44 -0500

    Added comments

--- grades.py
+++ grades.py
@@ -1,12 +1,12 @@
-grade = int(input("Input your percentage here: "))
+grade = int(input("Input your percentage here: ")) #assigning a variable and turning the input to an integer
 
-if grade > 59 and grade < 73:
+if grade > 59 and grade < 73: #Conditions for getting a D
     print("You got a D")
-elif grade > 73 and grade < 80:
+elif grade > 73 and grade < 80: #conditions for getting a C
     print("You got a C")
-elif grade > 80 and grade < 90:
+elif grade > 80 and grade < 90: #Conditions for getting a B
     print("You got a B")
-elif grade > 90:
+elif grade > 90: #conditions for getting an A
     print("You got an A")
-else:
+else: #if none of the numbers were inputed, it automatically becomes an F
     print("You got an F")
