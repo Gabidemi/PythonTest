b4430f41c699126d5e813261bf99ec6845cfc2c1
Gabidemi <gabidemi@brooklynsteamcenter.org>
2022-11-15 10:34:28 -0500

    loops

---
+++ Loops.py
@@ -0,0 +1,22 @@
+# # x = "Asta"
+
+
+# # while x != "stop":
+# #      print("hello world")
+# #      x = input("Type stop to exit")
+
+
+# # y = 1
+
+# # while y <= 10:
+# #     print( y )
+# #     y = y + 1
+
+
+# list = ["Apples", "Bananas", "Peaches"]
+
+# for x in list:
+#     print(x)
+
+for i in range(1,11):
+    print(i)
