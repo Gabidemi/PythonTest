70aa0c2dda363897463f9fdd9ac0ea04efd0869c
Gabidemi <gabidemi@brooklynsteamcenter.org>
2022-11-15 09:32:27 -0500

    lists

---
+++ lists.py
@@ -0,0 +1,20 @@
+List = ["Rimuru", "Naofumi", "Raphtalia", "Meliodas", "Monke"]
+
+print(List)
+
+List.append("Tarmiel") #add an item to a list
+
+print(List)
+
+print(List[2]) #picks an item from a list
+
+print(List[-1]) #prints the last item in a list
+
+List[0] = "Light" #Renaming something in a list
+print(List)
+
+del List[2] #Deletes an item from a list
+print(List)
+
+Tuple = ("Genshin", "7DS", "ME3", "Duel Links", "Dear Days") #Tuple list
+
