b30b5f8eccfa514c4e5e0ed497676538122ea9e1
Gabidemi <gabidemi@brooklynsteamcenter.org>
2022-11-15 10:54:55 -0500

    testing

---
+++ todo.py
@@ -0,0 +1,15 @@
+
+todolist = []
+
+x = input("What do you want to add to list?")
+todolist.append(x)
+
+print(todolist)
+
+
+while x = x:
+    x = input("what else?")
+
+    todolist.append(x)
+
+    print(todolist)
