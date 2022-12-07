f461b6d7eda3c6e91371e9586fc2fdfb2ccddacd
Gabidemi <gabidemi@brooklynsteamcenter.org>
2022-12-07 10:24:21 -0500

    Trying the Try and except

---
+++ Try.py
@@ -0,0 +1,18 @@
+try:
+    age = int(input('Enter your age: '))
+
+    faveNum = int(input('What is your favorite number: '))
+
+    if age <= 21:
+        print('You are not allowed to enter, you are too young.')
+    else:
+        print('Welcome, you are old enough.')
+    
+    print("Fun Fact! Your age divided by your favorite number is: " , age / faveNum)
+    
+except ValueError:
+    print("Please, for the love of GOD, use an actual number")
+except ZeroDivisionError:
+    print("PLEASE!! stop entering 0 ( zero )")
+finally:
+    print("This runs no matter what")
